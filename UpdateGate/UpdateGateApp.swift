18edd3f3f86087312b7fffe599812684a44d89fe
Quang Tran <quang.tran@appetiser.com.au>
2023-06-11 10:35:05 +0700

    Initial Commit

---
+++ UpdateGate/UpdateGateApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpdateGateApp.swift
+//  UpdateGate
+//
+//  Created by Anthony Tran on 11/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct UpdateGateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
